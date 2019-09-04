204f0769b6f8c298fce0dcb33d9904f334d47fc4
Mateusz-PC\Mateusz <mateuszkropisz@op.pl>
2019-09-04 20:52:51 +0200

    Mateusz - moje testowe zmiany.

--- ProjektPS/Startup.cs
+++ ProjektPS/Startup.cs
@@ -29,7 +29,7 @@ namespace ProjektPS
 
             services.AddDbContext<ProjektPSDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
 
-            services.AddMvc();
+            services.AddMvc(); --
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
